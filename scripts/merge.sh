e2fbe1de62cd995aa205947985a44b85e8a79263
Andrew Wei <andrewscwei@gmail.com>
2016-08-10 15:08:44 -0700

    Update scripts

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -2,20 +2,24 @@
 
 set -e
 
-if [ "$1" != "development" ]; then
-  git checkout development
-  git merge $1
-  git push
-fi
+if [ "$1" != "development" && "$1" != "staging" && "$1" != "master"]; then
+  echo -e "Unknown branch to merge: $1"
+else
+  if [ "$1" != "development" ]; then
+    git checkout development
+    git merge $1
+    git push
+  fi
 
-if [ "$1" != "staging" ]; then
-  git checkout staging
-  git merge $1
-  git push
-fi
+  if [ "$1" != "staging" ]; then
+    git checkout staging
+    git merge $1
+    git push
+  fi
 
-if [ "$1" != "master" ]; then
-  git checkout master
-  git merge $1
-  git push
+  if [ "$1" != "master" ]; then
+    git checkout master
+    git merge $1
+    git push
+  fi
 fi
