71572ceaa49cb4dee6cc4a841dfce2978a29d1c4
Andrew Wei <andrewscwei@gmail.com>
2018-04-19 01:13:40 -0700

    Update scripts

--- .circleci/get-opts.sh
+++ .circleci/get-opts.sh
@@ -4,4 +4,3 @@ PACKAGE_NAME=$(cat package.json | jq -r ".name")
 PACKAGE_VERSION=$(cat package.json | jq -r ".version")
 PACKAGE_FILE=$PACKAGE_NAME-$PACKAGE_VERSION.zip
 GIT_ORIGIN_URL=`git config --get remote.origin.url`
-IS_RELEASE=`[[ $(git describe --tags) =~ ^v[0-9]+(\.[0-9]+)+(-rc[0-9]+)?(-alpha[0-9]+)?$ ]] && echo true || echo false`
