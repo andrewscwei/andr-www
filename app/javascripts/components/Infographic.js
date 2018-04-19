2e7ea39a5c48b14d67b6f126aea27169ef10f2b1
Andrew Wei <andrewscwei@gmail.com>
2018-04-19 00:23:00 -0700

    Test new build pipeline

--- app/javascripts/components/Infographic.js
+++ app/javascripts/components/Infographic.js
@@ -3,6 +3,9 @@ import _ from 'lodash';
 import m, { DirtyType, Element } from 'meno';
 import request from 'superagent';
 
+const GITHUB_API_URL = `https://api.github.com`;
+const REQUEST_ACCEPT_HEADER = `application/vnd.github.v3+json`;
+
 class Infographic extends Element(HTMLCanvasElement, `x-infographic`) {
   static get extends() { return `canvas`; }
 
@@ -16,7 +19,16 @@ class Infographic extends Element(HTMLCanvasElement, `x-infographic`) {
   }
 
   init() {
-
+    const res = request
+      .get(`${GITHUB_API_URL}/user/repos`)
+      .set(`Accept`, REQUEST_ACCEPT_HEADER)
+      .set(`Authorization`, `token 5804cee1658cbd7c1311b4629e5bca3946a2ff05`)
+      .query({ visibility: `all` })
+      .query({ per_page: 100 })
+      .query({ affiliation: `owner,collaborator` })
+      .then(res => {
+        console.log(res.body);
+      });
   }
 
   destroy() {
